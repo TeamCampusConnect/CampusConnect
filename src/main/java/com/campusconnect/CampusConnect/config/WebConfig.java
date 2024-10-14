76c509c8fb316cf9eb8316343a68acd68ada7af7
HimanshuGolani <golanihimanshu@gmail.com>
2024-10-14 23:01:54 +0530

    origin changes in config

--- src/main/java/com/campusconnect/CampusConnect/config/WebConfig.java
+++ src/main/java/com/campusconnect/CampusConnect/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,19 +14,17 @@ public class WebConfig {
     public WebMvcConfigurer corsConfig(){
         return new WebMvcConfigurer(){
             public void addCorsMappings(CorsRegistry registry){
-               registry.addMapping("/**")
-                       .allowedOrigins("*")
-                       .allowedMethods(HttpMethod.GET.name(),
-                                        HttpMethod.POST.name(),
-                                        HttpMethod.DELETE.name(),
-                                        HttpMethod.PUT.name())
-                       .allowedHeaders(HttpHeaders.CONTENT_TYPE,
-                                       HttpHeaders.AUTHORIZATION)
-                       .allowCredentials(true)
-                       .exposedHeaders(HttpHeaders.AUTHORIZATION);
+                registry.addMapping("/**")
+                        .allowedOrigins("https://campusconnect-0o3k.onrender.com", "http://192.168.29.154:8080")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.PUT.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                HttpHeaders.AUTHORIZATION)
+                        .allowCredentials(true)
+                        .exposedHeaders(HttpHeaders.AUTHORIZATION);
             }
-
         };
     }
-
 }
