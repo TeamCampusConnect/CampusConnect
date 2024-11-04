fd8786bc8a7b4f07b1153181272dcea1455282ca
TeamCampusConnect <campusconnect.team.common@gmail.com>
2024-11-04 21:44:33 +0530

    changes in chat entity

--- src/main/java/com/campusconnect/CampusConnect/entity/ChatEntity.java
+++ src/main/java/com/campusconnect/CampusConnect/entity/ChatEntity.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class ChatEntity {
 
     @Id
-    @DBRef
     private ObjectId id;
 
     @DBRef
--- src/main/java/com/campusconnect/CampusConnect/entity/MessageEntity.java
+++ src/main/java/com/campusconnect/CampusConnect/entity/MessageEntity.java
@@ -1,6 +1,7 @@
 package com.campusconnect.CampusConnect.entity;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
